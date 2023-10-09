5ef5cc839a037946d39adeec968e7d1fe1a3c9c9
Goodonic <orexovheketa@gmail.com>
2023-10-09 18:47:31 +0500

    Test

--- managers/quests.js
+++ managers/quests.js
@@ -23,7 +23,7 @@ router.get('/parks/:name/Quests/', (req, res) => {
     let name = url[2];
     db.ref(`parks/${name}/Quests/`).once('value',
         (snapShot)=>{
-            console.log(snapShot.val());
+            //console.log(snapShot.val());
             res.json(snapShot.val())
         },
         (errorObject) => {
@@ -56,44 +56,47 @@ router.delete('/parks/:name/Quests/:ind', (req, res) => {
 })
 
 // TODO Изменения
-router.get('/:parkName/:Coast/:Code/:Description/:IsStandartQuest/:TaskName', (req, res) => {
+router.post('/:parkName', (req, res) => {
     console.log("рабоает")
     let url = req.url.split('/');
     let name = url[1];
     let newQuest;
-    // newQuest = {
-    //     Coast: Number(req.body.Coast) ,
-    //     Code: req.body.Code,
-    //     Description: req.body.Description,
-    //     IsStandartQuest: req.body.IsStandartQuest,
-    //     TaskName: req.body.TaskName,
-    // };
     newQuest = {
-        Coast: Number(url[2]) ,
-        Code: url[3],
-        Description: url[4],
-        IsStandartQuest: url[5],
-        TaskName: url[6],
+        Coast: Number(req.body.Coast) ,
+        Code: req.body.Code,
+        Description: req.body.Description,
+        IsStandartQuest: req.body.IsStandartQuest,
+        TaskName: req.body.TaskName,
     };
+    // newQuest = {
+    //     Coast: Number(url[2]) ,
+    //     Code: url[3],
+    //     Description: url[4],
+    //     IsStandartQuest: url[5],
+    //     TaskName: url[6],
+    // };
     console.log("Ща будет записть\n", newQuest);
     try {
         db.ref(`parks/${name}/Quests/${newQuest.TaskName}`).set(newQuest, function (error) {
             if (error) {
-                return res.sendStatus(404)
+                // return
+                //res.sendStatus(404)
                 // The write failed...
-                //console.log("Failed with error: " + error)
+                console.log("Failed with error: " + error)
             } else {
-                return res.sendStatus(200)
+                // return
+                //return res.sendStatus(200)
                 // The write was successful...
-                //console.log("success\n", ind)
+                console.log("success\n", ind)
             }
         })
     }
    catch (err){
        if (err) {
-           return res.sendStatus(404)
+           // return
+           //res.sendStatus(404)
            // The write failed...
-           //console.log("Failed with error: " + error)
+           console.log("Failed with error: " + error)
        }
     }
        //console.log(err)}
